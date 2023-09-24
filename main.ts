a598179554b14ffd4dde2410b2d01a1c53880f67
kirisk12 <145893002+kirisk12@users.noreply.github.com>
2023-09-24 13:46:32 +0700

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    hello = 0
+    _12 = 12
+    basic.showString("" + (_12))
+})
+input.onButtonPressed(Button.B, function () {
+    _12 = _12 + 1
+    basic.showString("" + (_12))
+})
+let hello = 0
+let _12 = 0
+_12 = 12
+hello = 1
+if (hello == 1) {
+    while (hello == 1) {
+        basic.showString("Hello!")
+    }
+} else {
+	
+}
